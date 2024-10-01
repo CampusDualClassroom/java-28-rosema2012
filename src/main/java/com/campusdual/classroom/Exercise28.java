3475796cd0b22be41224646828078d1905c74820
Happy Days!!! <36199346+rosema2012@users.noreply.github.com>
2024-10-01 16:18:01 +0200

    Ejercicio 28 lanzamiento de excepciones terminado

--- src/main/java/com/campusdual/classroom/Exercise28.java
+++ src/main/java/com/campusdual/classroom/Exercise28.java
@@ -1,7 +1,11 @@
 package com.campusdual.classroom;
 
+import java.io.IOException;
+
 public class Exercise28 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        throw new IOException("Exception not thrown");
 
     }
 }
